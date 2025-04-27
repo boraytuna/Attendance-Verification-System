000fbbaaf89c310b61f6959f3e2781899d36496e
boraytuna <boray_tuna61@hotmail.com>
2025-04-27 14:52:13 -0400

    Added openpyxl to requirements

--- app.py
+++ app.py
@@ -1538,6 +1538,6 @@ def init_db_route():
 
     return "Database initialized!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 
